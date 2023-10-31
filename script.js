65cb5a74fe4b437cc183717544a2751be023b672
Vincent Testagrossa <lancevancevt@gmail.com>
2023-10-31 10:48:46 -0400

    added event listeners to the cells that changes the background color when clicked. added a toggle for mouse drag detection. Modified the cells to auto-adjust to the container based on the gridsize.

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const container = document.getElementById('container');
+const containerSize = 960;
+let clicked = false;
+let gridSizeX = 50;
+let gridSizeY = 50;
+let RGB = "FF0000;"
+let cellColor = "background-color: #" + RGB;
+const cellStyle = "width: " + (containerSize / gridSizeX) + "px;" + " height: " + (containerSize / gridSizeY) + "px;";
 
-let gridSizeX = 16;
-let gridSizeY = 16;
+//Allows dragging.
+document.addEventListener('mousedown', () => {clicked = true;});
+document.addEventListener('mouseup', () => {clicked = false});
+
 function makeGrid(sizeX, sizeY){
     //Loop through the rows
     for (let y = 0; y < sizeY; y++){
@@ -13,6 +22,20 @@ function makeGrid(sizeX, sizeY){
         for(let x = 0; x < sizeX; x++){
             let gridCell = document.createElement('div');
             gridCell.classList.add('grid-square');
+            gridCell.setAttribute('style', cellStyle);
+            gridCell.addEventListener('mousedown', () => {
+                clicked = true;
+                gridCell.setAttribute("style", cellStyle + cellColor)
+            });
+            gridCell.addEventListener('mouseup', () => {
+                clicked = false;
+            });
+            gridCell.addEventListener('mouseenter', () => {
+                //Checks for mousedown before filling the tile
+                if (clicked){
+                    gridCell.setAttribute("style", cellStyle + cellColor)
+                }
+            });
             gridRow.appendChild(gridCell);
         }
     }
